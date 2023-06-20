00734dd6aa474c12f64008b404411a69348ac814
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-19 18:16:52 -1000

    [ADD] mostrando la seccion de testimoniales en el inicio

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -4,14 +4,20 @@ import { Testimonial } from '../models/Testimoniales.js';
 
 const paginaInicio = async (req, res) => {
 
+  const promiseDB = [];
+
+  promiseDB.push(Viaje.findAll({ limit: 3 }));
+  promiseDB.push(Testimonial.findAll({ limit: 3 }));
+
   try {
     // Obtenamos los testimoniles de la bd
-    const viajes = await Viaje.findAll({ limit: 3 });
+    const resultado = await Promise.all(promiseDB);
 
     res.render('../view/inicio.pug', {
       pagina: 'Inicio',
       clase: 'home',
-      viajes,
+      viajes: resultado[0],
+      testimoniales: resultado[1],
     });
   } catch (error) {
     console.log(error);
